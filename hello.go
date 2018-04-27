07f53e9e643fb1b651255822f267402b7a457ee3
nancy <xin080124@hotmail.com>
2018-04-27 20:03:21 +1200

    add tests

---
+++ hello.go
@@ -0,0 +1,82 @@
+//hello.go   
+package main
+// import "fmt"//实现格式化的I/O  
+
+import ( 
+    "encoding/json" 
+    "fmt" 
+    "io" 
+    "log" 
+    "strings"
+    "os" 
+) 
+ 
+
+func testDecoder(){
+	const jsonStream = ` 
+        { "Name" : "Ed" , "Text" : "Knock knock." } 
+        { "Name" : "Sam" , "Text" : "Who's there?" } 
+        { "Name" : "Ed" , "Text" : "Go fmt." } 
+        { "Name" : "Sam" , "Text" : "Go fmt who?" } 
+        { "Name" : "Ed" , "Text" : "Go fmt yourself!" } 
+        `
+
+        type Message struct { 
+        	Name , Text string 
+    	} 
+
+		dec := json. NewDecoder ( strings. NewReader ( jsonStream ) ) 
+		    for { 
+		        var m Message 
+		        if err := dec. Decode ( & m ) ; err == io. EOF { 
+		            break 
+		        } else if err != nil { 
+		            log . Fatal ( err ) 
+		        } 
+		        fmt. Printf ( "%s: %s \n " , m. Name , m. Text ) 
+		    } 
+
+       fmt.Printf("Hello,GO!\n")   
+
+}
+
+func testMarshal(){
+	type ColorGroup struct { 
+        ID     int 
+        Name   string 
+        Colors [ ] string 
+    } 
+    group := ColorGroup { 
+        ID :     1 , 
+        Name :   "Reds" , 
+        Colors : [ ] string { "Crimson" , "Red" , "Ruby" , "Maroon" } , 
+    } 
+    b , err := json. Marshal ( group ) 
+    if err != nil { 
+        fmt. Println ( "error:" , err ) 
+    } 
+    os. Stdout . Write ( b ) 
+}
+
+func testUnMarshal(){
+	var jsonBlob = [ ] byte ( ` [ 
+        { "Name" : "Platypus" , "Order" : "Monotremata" } , 
+        { "Name" : "Quoll" ,     "Order" : "Dasyuromorphia" } 
+    ] ` ) 
+    type Animal struct { 
+        Name  string 
+        Order string 
+    } 
+    var animals [ ] Animal 
+    err := json. Unmarshal ( jsonBlob , & animals ) 
+    if err != nil { 
+        fmt. Println ( "error:" , err ) 
+    } 
+    fmt. Printf ( "%+v" , animals ) 
+}
+/*Printf someting*/  
+func main(){   
+	testDecoder()
+	testMarshal()
+	testUnMarshal()
+}
